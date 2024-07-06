cf8b52a1ffabf20ad24eedb91bed65b9038c372b
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-06 21:51:08 +0900

    general purpose

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,44 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-# ...
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    # same as above, no changes needed.
+    ...
+
+# from django.db.models import F
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.shortcuts import get_object_or_404, render
+# from django.urls import reverse
+
+# from .models import Choice, Question
+
+
+# # ...
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -29,37 +61,37 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
     
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-# from django.http import HttpResponse
-# from django.http import Http404
-# from django.template import loader
-# from django.shortcuts import render
-# from django.shortcuts import get_object_or_404, render
-# from .models import Question
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
+# # from django.http import HttpResponse
+# # from django.http import Http404
+# # from django.template import loader
+# # from django.shortcuts import render
+# # from django.shortcuts import get_object_or_404, render
+# # from .models import Question
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
 
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/detail.html", {"question": question})
+# # def detail(request, question_id):
+# #     question = get_object_or_404(Question, pk=question_id)
+# #     return render(request, "polls/detail.html", {"question": question})
