8407610cf014c29d045192a8c10680a06bf280e5
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-08 19:26:22 +0900

    change question and choice

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,17 +2,25 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
-
-# class QuestionAdmin(admin.ModelAdmin):
-#     fields = ["pub_date", "question_text"]
+from .models import Choice, Question
 
 
-# admin.site.register(Question, QuestionAdmin)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
+
+
+
+# ...
+
