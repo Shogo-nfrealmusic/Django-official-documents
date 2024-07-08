76b82c289e461fc403688ded937373a0a681e89e
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-08 19:22:29 +0900

    style of admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ["pub_date", "question_text"]
+
+
+# admin.site.register(Question, QuestionAdmin)
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
