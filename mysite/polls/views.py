1e46c47f57b70ca5e25243d67b7722462016ccdc
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-08 18:48:27 +0900

    strict future date

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,15 @@ from django.http import HttpResponse
 from django.utils import timezone
 from .models import Choice, Question
 
+class DetailView(generic.DetailView):
+    ...
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
