5eed741d8b70019d0df667767a359309c32aebf4
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-09 20:17:38 +0900

    django debuggur

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
+STATIC_URL = "static/"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 app_name = "polls"
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-]
+] + debug_toolbar_urls()
